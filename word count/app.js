7e3fd015774ca67006e6d1b04ef48707f9ee1b6d
Dharni73 <113425057+Dharni73@users.noreply.github.com>
2023-08-04 19:18:07 +0530

    Add files via upload

---
+++ word count/app.js
@@ -0,0 +1,8 @@
+function Count(){
+
+const words = document.getElementById("input").value
+const counts = words.split(' ')
+const Results = document.getElementById("result")
+result.innerHTML = counts.length
+
+}
